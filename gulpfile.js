efdd4cc45a63b0d679dc66b0a07b21688f2166e3
Flat-Pixels <steeveverdieu@gmail.com>
2018-10-25 15:51:58 -0400

    chore(Gulpfile.js): Add browser-sync
    Adding browser-sync to watch files changes and update the web browser
    page.

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,40 @@
 
 var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
+var browserSync = require('browser-sync').create();
 
 sass.compiler = require( 'node-sass' );
 
 
+/*
+* Task that compile the sass files
+*/
 gulp.task( 'sass', function(){
   return gulp.src( './assets/styles/scss/**/*.scss' )
     .pipe( sass.sync( {outputStyle: 'expanded'} ).on( 'error', sass.logError ) )
-    .pipe( gulp.dest( './assets/styles' ) );
+    .pipe( gulp.dest( './assets/styles' ) )
+    .pipe(browserSync.stream());
 });
 
-gulp.task( 'sass:watch', function() {
-  gulp.watch( './assets/styles/scss/**/*.scss', [ 'sass' ] )
-})
 
-gulp.task( 'default', [ 'sass:watch' ], function(){
-  //Do nothing here...
+/*
+* Task that watch all sass files
+* Call the task to compile them
+* Wacth the html files changes and reload the whole page
+*/
+gulp.task( 'watch', function() {
+  gulp.watch( './assets/styles/scss/**/*.scss', [ 'sass' ] );
+  gulp.watch( './**/*.html').on( 'change', browserSync.reload );
+});
+
+/*
+* Default task
+*/
+gulp.task( 'default', [ 'watch' ], function(){
+
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
 });
