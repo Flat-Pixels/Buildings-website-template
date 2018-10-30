1f1f959d334fa78695249f117ffb632dd25a0d0d
Flat-Pixels <steeveverdieu@gmail.com>
2018-10-30 15:08:25 -0400

    fix(html/css): Fix html & css for smacss
    Clean the html & css to get a mantainable code.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,33 @@
 
+/*
+* ---------------------------------------------
+* ---------------------------------------------
+* ---------------------------------------------
+*
+* Don't touch to this file
+* Unless you really know what you are doing
+*
+*
+* ---------------------------------------------
+* ---------------------------------------------
+*/
 
 
-
-(function($) {    
-   // other code here    
+/*
+*
+* This show and hide the site main navigation
+* When the browser is a mobile one
+* And toggle the overflow of the body of the page
+*
+*/
+(function($) {  
  $(document).ready(function() {   
     $( '.nav-mobile-btn' ).on( 'click', function() {
+
+      //Toggle whit the visibility of the mebu
       $( '.nav-primary' ).toggleClass( 'visible' );
 
+      //Show or Hide the body ouverflow
       if( $( ".nav-primary" ).hasClass( "visible" ) ) {
         $('html, body').css('overflowY', 'hidden'); 
       }else {
